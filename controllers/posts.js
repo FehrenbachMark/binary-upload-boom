1106649555c529b6279024e4c7090725730eba82
FehrenbachMark <fehrenbachmark@gmail.com>
2022-10-02 19:33:19 -0400

    added usernames associated with posts to feed and posts pages

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,5 +1,6 @@
 const cloudinary = require("../middleware/cloudinary");
 const Post = require("../models/Post");
+const Comment = require("../models/Comment");
 
 module.exports = {
   getProfile: async (req, res) => {
@@ -13,7 +14,7 @@ module.exports = {
   getFeed: async (req, res) => {
     try {
       const posts = await Post.find().sort({ createdAt: "desc" }).lean();
-      res.render("feed.ejs", { posts: posts, user: req.user });
+      res.render("feed.ejs", { posts: posts });
     } catch (err) {
       console.log(err);
     }
@@ -21,7 +22,8 @@ module.exports = {
   getPost: async (req, res) => {
     try {
       const post = await Post.findById(req.params.id);
-      res.render("post.ejs", { post: post, user: req.user });
+      const comments = await Comment.find({post: req.params.id}).sort({ createdAt: "desc" }).lean();
+      res.render("post.ejs", { post: post, user: req.user.id, userName: req.user.userName, comments: comments });
     } catch (err) {
       console.log(err);
     }
@@ -37,9 +39,11 @@ module.exports = {
         cloudinaryId: result.public_id,
         caption: req.body.caption,
         likes: 0,
+        userName: req.user.userName,
         user: req.user.id,
       });
       console.log("Post has been added!");
+      console.log(req.user.userName);
       res.redirect("/profile");
     } catch (err) {
       console.log(err);
--- routes/posts.js
+++ routes/posts.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const upload = require("../middleware/multer");
 const postsController = require("../controllers/posts");
+const commentsController = require("../controllers/comments");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Post Routes - simplified for now
@@ -10,8 +11,9 @@ router.get("/:id", ensureAuth, postsController.getPost);
 router.post("/createPost", upload.single("file"), postsController.createPost);
 
 router.put("/likePost/:id", postsController.likePost);
-
 router.put("/unlikePost/:id", postsController.unlikePost);
+
+router.delete("/deleteComment/:id", commentsController.deleteComment);
 
 router.delete("/deletePost/:id", postsController.deletePost);
 
